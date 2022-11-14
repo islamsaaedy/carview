48a03bf5c454ad2cbcb108ebc4d318c5ca0a0fc6
Eslam Saeed Mohamed <102478392+islamsaaedy@users.noreply.github.com>
2022-11-15 01:55:57 +0200

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,9 @@
+// let home = document.querySelector('.Home')
+// window.onscroll = function () {
+//     if (window.scrollY >= 500 && window.scrollY <= 1776) {
+//         home.style.opacity = "1"
+//     }
+//     else {
+//         home.style.opacity = "0"
+//     }
+// }
